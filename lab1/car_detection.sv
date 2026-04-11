345e08867f771861cdfa01d82c569053aecf7314
CiciZhang11 <zpx2006@uw.edu>
2026-04-10 17:18:46 -0700

    add comments for car detection and a minor mistake

--- lab1/car_detection.sv
+++ lab1/car_detection.sv
@@ -1,10 +1,12 @@
+// This module is to detect if a car just entered or exited a parking space. It takes in the inputs of 2 sensors alongside with
+// clock and reset signal, and output signal indicating if the car entered or exited.
 module car_detection(clk, reset, outer, inner, enter, exit);
     input  logic clk, reset, outer, inner;
     output logic enter, exit;
     
-    typedef enum logic [2:0] {IDLE, enter1, enter2, enter3, exit1, exit2, exit3} state_t;
+    typedef enum logic [3:0] {IDLE, enter1, enter2, enter3, exit1, exit2, exit3, E_DONE, X_DONE} state_t;
     state_t ps, ns;
-    
+    // Combinational logic for next state and output generation
     always_comb begin
         ns = ps;    // Default: stay in current state
         enter = 1'b0;
@@ -56,23 +58,26 @@ module car_detection(clk, reset, outer, inner, enter, exit);
                 if(!outer && !inner) ns = X_DONE; // Sequence complete
                 else if(outer && inner) ns = exit2;
 					 else ns = exit3;
-            end
+			end //end exit3 case
 
             X_DONE: begin
                 exit = 1'b1; 
                 ns = IDLE; 
-            end
+			end //end X_DONE case
             
             default: ns = IDLE; 
             
         endcase //end listing ps cases 
-    end //end combinational logic
-    
+    end //end always_comb
+
+	
+	//Sequential logic to update state memory on clock edge. if reset, go to IDLE(initial state)
+	//else, go to the next state.
     always_ff @(posedge clk) begin
         if(reset)
             ps <= IDLE;
         else
             ps <= ns;
-    end //end sequential logic of flip-flop
+    end //end always_ff
     
 endmodule // end module
