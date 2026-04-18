1d78d9ca611de092f5da2f67da7b933dbd63b3e8
CiciZhang11 <zpx2006@uw.edu>
2026-04-17 21:57:56 -0700

    Add files via upload

--- lab2/task1_tb.sv
+++ lab2/task1_tb.sv
@@ -1,24 +1,24 @@
-'timescale 1 ps / 1 ps
+`timescale 1 ps / 1 ps
 module task1_tb();
-	logic clk;
 	logic [4:0] address;
+	logic clk;
 	logic [2:0] data;
 	logic wren;
-	logic [2:0] dout;
-	
+	logic [2:0] dout1;
+	logic [2:0] dout2;
 	task1 dut1 (
-		.clk(clk),
 		.address(address),
+		.clk(clk),
 		.data(data),
 		.wren(wren),
-		.dout(dout)
+		.dout(dout1)
 	);
 	task2 dut2 (
-		.clk(clk),
 		.address(address),
+		.clk(clk),
 		.data(data),
 		.wren(wren),
-		.dout(dout)
+		.dout(dout2)
 	);
 	parameter period = 100;
 	initial begin
@@ -27,14 +27,16 @@ module task1_tb();
 	end  // initial clk
 
 	initial begin
-		wren = 0; address = 5'd0; data = 3'd0; @(posedge clk); 
+		wren = 0; address = 5'd0; data = 3'd0; @(posedge clk); 
 		wren = 1; address = 5'd5; data = 3'd5; @(posedge clk); // test writing 5 to address 5
 		wren = 1; address = 5'd12; data = 3'd3; @(posedge clk); // test writing 3 to address 12
 		wren = 0; address = 5'd5; data = 3'd0; @(posedge clk); // test reading address 5 (dout should be 5)
 		wren = 0; address = 5'd12; data = 3'd0; @(posedge clk); // test reading address 12 (dout should be 3)
 		wren = 0; address = 5'd0; data = 3'd0; @(posedge clk); // test reading address 0 (dout should be x or 0)
+		wren = 1; address = 5'd12; data = 3'd2; @(posedge clk);
 		wren = 0; address = 5'd0; data = 3'd0; repeat(5) @(posedge clk);
 						
 		$stop; // end simulation
-	end  // end initial
-endmodule // end task1_tb module
+	
+end // end task1_tb module
+endmodule
