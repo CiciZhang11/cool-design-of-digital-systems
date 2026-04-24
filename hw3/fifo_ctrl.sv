f7ff4d48668e8d5bdfcf93b461bcf0eae614727d
Jiaze-G <jiazeg@uw.edu>
2026-04-24 15:01:19 -0700

    Add files via upload

--- hw3/fifo_ctrl.sv
+++ hw3/fifo_ctrl.sv
@@ -4,16 +4,17 @@
  * signals empty and full.
  */
 module fifo_ctrl #(parameter ADDR_WIDTH=4)
-                 (clk, reset, rd, wr, empty, full, w_addr, r_addr);
+                 (clk, reset, rd, wr, empty, full, w_addr, r_addr, half_sel);
   
   input  logic clk, reset, rd, wr;
-  output logic empty, full;
+  output logic empty, full, half_sel; // add a output half_sel
   output logic [ADDR_WIDTH-1:0] w_addr, r_addr;
   
   // signal declarations
   logic [ADDR_WIDTH-1:0] rd_ptr, rd_ptr_next;
   logic [ADDR_WIDTH-1:0] wr_ptr, wr_ptr_next;
   logic empty_next, full_next;
+  logic half_sel_next; // next state of half_sel
   
   // output assignments
   assign w_addr = wr_ptr;
@@ -27,6 +28,7 @@ module fifo_ctrl #(parameter ADDR_WIDTH=4)
         rd_ptr <= 0;
         full   <= 0;
         empty  <= 1;
+		  half_sel <= 0; // when rest is on, half_sel = 0, selecting upper half
       end
     else
       begin
@@ -34,6 +36,7 @@ module fifo_ctrl #(parameter ADDR_WIDTH=4)
         rd_ptr <= rd_ptr_next;
         full   <= full_next;
         empty  <= empty_next;
+		  half_sel <= half_sel_next;
       end
   end  // always_ff
   
@@ -44,20 +47,41 @@ module fifo_ctrl #(parameter ADDR_WIDTH=4)
     wr_ptr_next = wr_ptr;
     empty_next = empty;
     full_next = full;
+	 half_sel_next = half_sel;
+	 
     case ({rd, wr})
-      2'b11:  // read and write
-        begin
-          rd_ptr_next = rd_ptr + 1'b1;
-          wr_ptr_next = wr_ptr + 1'b1;
+
+      2'b11: begin  // read and write
+        if (~empty) begin
+          if (half_sel == 1'b0) begin
+            half_sel_next = 1'b1; // read upper half, stay same address
+          end else begin
+            half_sel_next = 1'b0; // read lower half
+            rd_ptr_next = rd_ptr + 1'b1;
+          end // end else
         end
-      2'b10:  // read
+
+        if (~full) begin
+          wr_ptr_next = wr_ptr + 1'b1;
+          empty_next = 0;
+        end
+      end
+		
+      2'b10: begin  // read
         if (~empty)
           begin
-            rd_ptr_next = rd_ptr + 1'b1;
-            if (rd_ptr_next == wr_ptr)
-              empty_next = 1;
-            full_next = 0;
-          end
+            if (half_sel == 1'b0) begin
+              half_sel_next = 1'b1; // read upper half only
+            end else begin
+              half_sel_next = 1'b0; // read lower half
+              rd_ptr_next = rd_ptr + 1'b1; // remove entry
+              if (rd_ptr_next == wr_ptr)
+                empty_next = 1;
+              end
+				  full_next = 0;
+            end
+		end
+		
       2'b01:  // write
         if (~full)
           begin
