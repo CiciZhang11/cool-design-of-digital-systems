9788e7c5486da02d394e36d8a8ff4322c87e2226
Jiaze-G <jiazeg@uw.edu>
2026-05-01 22:35:32 -0700

    Add files via upload

--- lab3/DE1_SoC.sv
+++ lab3/DE1_SoC.sv
@@ -17,26 +17,14 @@ module DE1_SoC (
 );
 
     // Signal definitions
-    logic clk, reset;
-    assign clk   = CLOCK_50;          
-    assign reset = ~KEY[0]; 
+	logic clk, reset;
+	assign clk = CLOCK_50;
+    assign reset = ~KEY[0];
 
-    logic signed [23:0] audio_t1;
-    logic signed [23:0] audio_t2;
-    logic signed [23:0] audio_filtered;
+    logic [23:0] readdata_left, readdata_right;
+    logic [23:0] final_output;
+    logic read_ready, write_ready;
 
-    logic read_ready;
-    logic write_ready;
-    logic read;
-    logic write;
- 
-    // Instantiate Audio CODEC Interface (part 1)
-    part1 p1 (
-		.CLOCK_50(CLOCK_50), .CLOCK2_50(CLOCK2_50), .KEY(KEY),
-		.FPGA_I2C_SCLK(FPGA_I2C_SCLK), .FPGA_I2C_SDAT(FPGA_I2C_SDAT),
-		.AUD_XCK(AUD_XCK), .AUD_DACLRCK(AUD_DACLRCK), .AUD_ADCLRCK(AUD_ADCLRCK),
-		.AUD_BCLK(AUD_BCLK), .AUD_ADCDAT(AUD_ADCDAT), .AUD_DACDAT(AUD_DACDAT)
-    );
      
     // Instantiate Task 1
     task1 t1 (
@@ -54,24 +42,24 @@ module DE1_SoC (
     );
     
     // SW9 MUX
-    logic [23:0] audio_in;
-    assign audio_in = (SW[9] == 1'b0) ? audio_t1 : audio_t2;
+    assign mux9_out = (SW[9] == 1'b0) ? audio_t1 : audio_t2;
 
     // Instantiate Task 3
-	fir_filter #(.N(8), .ADDR_WIDTH(3), .LOG2_N(3)) t3 (
-		 .clk(clk), .reset(reset),
-		 .en(read_ready & write_ready),
-		 .sample_in(audio_in),
-		 .sample_out(audio_filtered)
-	);
+    fir_filter #(
+        .DATA_WIDTH(24),
+        .N(8),
+        .ADDR_WIDTH(3),
+        .LOG2_N(3)      
+    ) t3 (
+        .clk        (clk),
+        .reset      (reset),
+        .en         (read_ready & write_ready),
+        .sample_in  (mux9_out),
+        .sample_out (audio_filtered)
+    );
 
-     
     // SW8 MUX
-    logic [23:0] final_audio;
-    assign final_audio = (SW[8] == 1'b1) ? audio_filtered : audio_in;
+	assign final_audio = (SW[8] == 1'b1) ? audio_filtered : mux9_out;
 
-    // Connect to CODEC
-    assign read  = read_ready & write_ready;
-    assign write = read_ready & write_ready;
 
 endmodule // end module DE1_SoC
