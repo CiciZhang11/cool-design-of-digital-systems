0f8c960ef7ce86a58d447e36890d96531efbcbdd
CiciZhang11 <zpx2006@uw.edu>
2026-05-01 19:17:35 -0700

    Add files via upload

---
+++ lab3/fir_filter.sv
@@ -0,0 +1,56 @@
+// This is a filter. 
+module fir_filter #(
+    parameter DATA_WIDTH = 24, // audio sample width
+    parameter N = 8,    // number of samples to average
+    parameter ADDR_WIDTH = 3,
+    parameter LOG2_N = 3  // for arithmetic right-shift
+)(
+    input  logic clk,
+    input  logic reset,
+    input  logic en, 
+    input  logic [DATA_WIDTH-1:0] sample_in, // raw audio
+    output logic [DATA_WIDTH-1:0] sample_out // filtered audio
+);
+
+    // 1. divide sample_in by N using arithmetic right shift
+    logic signed [DATA_WIDTH-1:0] divided_in;
+    assign divided_in = {{LOG2_N{sample_in[DATA_WIDTH-1]}},
+                          sample_in[DATA_WIDTH-1 : LOG2_N]};
+	 // 2. FIFO
+	 // Use en for both read enable and write enable because we need to simultaously
+	 // update both
+    logic fifo_empty, fifo_full;
+    logic [DATA_WIDTH-1:0] divided_out;
+
+    fifo #(
+        .DATA_WIDTH (DATA_WIDTH),
+        .ADDR_WIDTH (ADDR_WIDTH)
+    ) sample_fifo (
+        .clk    (clk),
+        .reset  (reset),
+        .rd     (en), 
+        .wr     (en),  // use the same enable signal for both read and write
+        .empty  (fifo_empty),
+        .full   (fifo_full),
+        .w_data (divided_in), // push the newest
+        .r_data (divided_out) // get the oldest
+    );
+	 // 3. Applying accumulator.
+	 // update the same of the last N divided samples using 
+    // acc <= acc + divided_in + (~divided_out + 1)
+    logic signed [DATA_WIDTH-1:0] acc;
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            acc <= '0;
+        end // end if
+		  else if (en) begin
+            // Add new divided sample; subtract oldest via two's complement
+            acc <= acc + $signed(divided_in) + ($signed(~divided_out) + 1'b1);
+        end // end else if
+    end // end always_ff
+
+    // 4. output sample_out
+    assign sample_out = acc;
+
+endmodule  // fir_filter
